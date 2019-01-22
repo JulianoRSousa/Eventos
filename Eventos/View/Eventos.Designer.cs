a4294cba4bcd9feb7b18e3b0aaff52d041876c5e
JulianoRSousa <julianorodsousa@gmail.com>
2019-01-21 23:16:30 -0300

    Organização de Classes

--- Eventos/View/Eventos.Designer.cs
+++ Eventos/View/Eventos.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(543, 181);
+            this.button2.Location = new System.Drawing.Point(944, 300);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -41,21 +42,35 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(944, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Criar Feed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Eventos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1864, 1041);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
+            this.Location = new System.Drawing.Point(0, 20);
             this.Name = "Eventos";
             this.ShowIcon = false;
             this.Text = "      Eventos";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Eventos_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Eventos/View/Eventos.cs
+++ Eventos/View/Eventos.cs
@@ -17,6 +17,8 @@ namespace Eventos
         LoginController controller = new LoginController();
         EventosController EventosController = new EventosController();
         private MySqlClassConnection MyConn = new MySqlClassConnection();
+        int yFeed = 0;
+        int y = 1;
 
         public Eventos()
         {
@@ -40,8 +42,81 @@ namespace Eventos
             Application.Restart();
             Environment.Exit(0);
         }
-        
 
-        
+        private void Eventos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            adicionarFeed();
+        }
+
+        private int adicionarFeed()
+        {
+            criarFeed(yFeed);
+            yFeed = y * 260;
+            y++;
+            return yFeed;
+        }
+
+        public void criarFeed(int valory)
+        {
+            Panel panelFeed = new Panel();
+            Label labelFeedTitulo = new Label();
+            PictureBox pictureBoxFeedCapa = new PictureBox();
+            Label labelFeedDescricao = new Label();
+
+            // 
+            // labelFeedTitulo
+            // 
+            labelFeedTitulo.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            labelFeedTitulo.Location = new Point(10, 10);
+            labelFeedTitulo.Margin = new Padding(0);
+            labelFeedTitulo.MaximumSize = new Size(450, 35);
+            labelFeedTitulo.MinimumSize = new Size(90, 7);
+            labelFeedTitulo.Name = "labelFeedTitulo";
+            labelFeedTitulo.Size = new Size(450, 50);
+            labelFeedTitulo.TabIndex = 0;
+            labelFeedTitulo.Text = "Titulo do evento";
+            // 
+            // labelFeedDescricao
+            // 
+            labelFeedDescricao.Font = new Font("HoloLens MDL2 Assets", 15.75F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            labelFeedDescricao.Location = new Point(45, 45);
+            labelFeedDescricao.MaximumSize = new Size(415, 160);
+            labelFeedDescricao.MinimumSize = new Size(83, 32);
+            labelFeedDescricao.Name = "labelFeedDescricao";
+            labelFeedDescricao.Size = new Size(415, 160);
+            labelFeedDescricao.TabIndex = 0;
+            labelFeedDescricao.Text = "label1";
+            // 
+            // pictureBoxFeedCapa
+            // 
+            pictureBoxFeedCapa.Location = new Point(0, 30);
+            pictureBoxFeedCapa.Margin = new Padding(0);
+            pictureBoxFeedCapa.Name = "pictureBoxFeedCapa";
+            pictureBoxFeedCapa.Size = new Size(460, 190);
+            pictureBoxFeedCapa.TabIndex = 0;
+            pictureBoxFeedCapa.TabStop = false;
+            // 
+            // panelFeed
+            // 
+
+            panelFeed.Controls.Add(labelFeedTitulo);
+            panelFeed.Controls.Add(labelFeedDescricao);
+            panelFeed.Controls.Add(pictureBoxFeedCapa);
+            panelFeed.Location = new Point(5, yFeed);
+            panelFeed.Margin = new Padding(0);
+            panelFeed.MaximumSize = new Size(600, 250);
+            panelFeed.MinimumSize = new Size(120, 50);
+            panelFeed.Name = "panelFeed";
+            panelFeed.Size = new Size(600, 250);
+            panelFeed.TabIndex = 0;
+            panelFeed.BackColor = SystemColors.ActiveCaption;
+
+            this.Controls.Add(panelFeed);
+        }
     }
 }
